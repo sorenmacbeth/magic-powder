13849877204c1971b451b946efc3c10670f3489f
Vebjorn Ljosa <vljosa@yieldbot.com>
2017-08-29 16:42:51 -0400

    add :repositories to projects.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,14 @@
 (defproject yieldbot/magic-powder "0.1.0-SNAPSHOT"
+  :repositories [["snapshots"
+                  {:url "https://artifactory.yb0t.cc/artifactory/yieldbot-maven-snapshots"
+                   :username :env/LEIN_USER
+                   :password :env
+                   :sign-releases false}]
+                 ["releases"
+                  {:url "https://artifactory.yb0t.cc/artifactory/yieldbot-maven-releases"
+                   :username :env/LEIN_USER
+                   :password :env
+                   :sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
