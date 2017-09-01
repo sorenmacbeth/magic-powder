696b0cc5f1b64df92578431e6a5009b1512ec914
Vebjorn Ljosa <vljosa@yieldbot.com>
2017-09-01 17:59:08 -0400

    remove yieldbot repos from project.clj

--- project.clj
+++ project.clj
@@ -3,16 +3,6 @@
   :url "http://github.com/yieldbot/magic-powder"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories [["snapshots"
-                  {:url "https://artifactory.yb0t.cc/artifactory/yieldbot-maven-snapshots"
-                   :username :env/LEIN_USER
-                   :password :env
-                   :sign-releases false}]
-                 ["releases"
-                  {:url "https://artifactory.yb0t.cc/artifactory/yieldbot-maven-releases"
-                   :username :env/LEIN_USER
-                   :password :env
-                   :sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
