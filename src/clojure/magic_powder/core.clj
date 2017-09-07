9c55c01cb27e1a30e1b4abc43bb22a7fb8d4481a
Vebjorn Ljosa <vljosa@yieldbot.com>
2017-09-07 16:41:28 -0400

    Prevent race condition with a read--write lock

--- src/clojure/magic_powder/core.clj
+++ src/clojure/magic_powder/core.clj
@@ -1,13 +1,26 @@
 (ns magic-powder.core
   (:import java.io.Closeable
-           [magic_powder ClosedException Hashtable]))
+           [magic_powder ClosedException Hashtable]
+           [java.util.concurrent.locks ReentrantReadWriteLock]))
 
 (declare hash-table?)
 
-(defrecord HashTable [htpa]
+(defmacro with-lock [lock & body]
+  `(let [lock# ~lock]
+     (try
+       (.lock lock#)
+       ~@body
+       (finally
+         (.unlock lock#)))))
+
+(defmacro with-read-lock [ht & body]
+  `(with-lock (.readLock (:rwlock ~ht))
+     ~@body))
+
+(defrecord HashTable [htpa rwlock]
   java.io.Closeable
   (close [ht]
-    (locking (:htpa ht)
+    (with-lock (.writeLock rwlock)
       (when @(:htpa ht)
         (Hashtable/unmapHashtable @(:htpa ht))
         (reset! (:htpa ht) nil)))))
@@ -23,26 +36,30 @@
 (defn insert-bytes [ht k v]
   {:pre [(hash-table? ht)
          (string? k)]}
-  (check-closed ht)
-  (Hashtable/insert_bytes @(:htpa ht) k (byte-array v)))
+  (with-read-lock ht
+    (check-closed ht)
+    (Hashtable/insert_bytes @(:htpa ht) k (byte-array v))))
 
 (defn insert-doubles [ht k v]
   {:pre [(hash-table? ht)
          (string? k)]}
-  (check-closed ht)
-  (Hashtable/insert_doubles @(:htpa ht) k (double-array v)))
+  (with-read-lock ht
+    (check-closed ht)
+    (Hashtable/insert_doubles @(:htpa ht) k (double-array v))))
 
 (defn get-bytes [ht k]
   {:pre [(hash-table? ht)
          (string? k)]}
-  (check-closed ht)
-  (Hashtable/get_bytes @(:htpa ht) k))
+  (with-read-lock ht
+    (check-closed ht)
+    (Hashtable/get_bytes @(:htpa ht) k)))
 
 (defn get-doubles [ht k]
   {:pre [(hash-table? ht)
          (string? k)]}
-  (check-closed ht)
-  (Hashtable/get_doubles @(:htpa ht) k))
+  (with-read-lock ht
+    (check-closed ht)
+    (Hashtable/get_doubles @(:htpa ht) k)))
 
 (defn hash-table? [x]
   (instance? HashTable x))
@@ -54,8 +71,10 @@
 (defn create-hash-table
   [key-size value-size capacity nbuckets & [filename]]
   (HashTable. (atom (Hashtable/makeHashtable key-size value-size capacity nbuckets
-                                             filename))))
+                                             filename))
+              (ReentrantReadWriteLock.)))
 
 (defn open-hash-table
   [filename]
-  (HashTable. (atom (Hashtable/mapHashtable filename))))
+  (HashTable. (atom (Hashtable/mapHashtable filename))
+              (ReentrantReadWriteLock.)))
