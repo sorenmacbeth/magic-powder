fa41f26db1843ed29eac881d1ef79059ecc697ec
Vebjorn Ljosa <vljosa@yieldbot.com>
2017-09-08 13:28:05 -0400

    Version 0.3.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yieldbot/magic-powder "0.3.0"
+(defproject yieldbot/magic-powder "0.3.1-SNAPSHOT"
   :description "Clojure library for off-heap hash table backed by a memory-mapped file"
   :url "http://github.com/yieldbot/magic-powder"
   :license {:name "Eclipse Public License"
